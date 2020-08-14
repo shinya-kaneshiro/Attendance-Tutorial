371b62933e13acf58423e6d3ff3e07e3bb8be1a0
kaneyan <armageddn1@gmail.com>
2020-08-14 03:30:51 +0000

    Finish add-basic-info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,6 +48,12 @@ class UsersController < ApplicationController
   end
   
   def update_basic_info
+    if @user.update_attributes(basic_info_params)
+      flash[:success] = "#{@user.name}の基本情報を更新しました。"
+    else
+      flash[:danger] = "#{@user.name}の更新は失敗しました。<br>" + @user.errors.full_messages.join("<br>")
+    end
+    redirect_to users_url
   end
   
   private
@@ -56,6 +62,10 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
     end
     
+    def basic_info_params
+      params.require(:user).permit(:department, :basic_time, :work_time)
+    end
+  
     
     # beforeフィルター
     
